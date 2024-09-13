b6304da836cd122903e115d30c0d563fb1731cee
Alessandro Nunes Assuncao <nunes.1370451@gmail.com>
2024-09-13 09:04:41 -0300

    Adicionar arquivos de projeto.

---
+++ Models/WorkShop.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace APIQueroOficina.Models
+{
+    public class WorkShop
+    {
+        [Required]
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [Required]
+        [BsonElement("Name")]
+        public  string WorkShopName { get; set; }
+        [Required]
+        public string Adress { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        [Required]
+        public string Email { get; set; }
+        
+
+
+    }
+}
