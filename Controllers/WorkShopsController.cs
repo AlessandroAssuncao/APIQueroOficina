68ab84f953451cd547fc373e313a44eb2255f9bb
Alessandro Nunes Assuncao <nunes.1370451@gmail.com>
2024-09-13 11:40:51 -0300

    Crud Finalizado

---
+++ Controllers/WorkShopsController.cs
@@ -0,0 +1,63 @@
+﻿using APIQueroOficina.Services;
+using APIQueroOficina.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace APIQueroOficina.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WorkShopsController : ControllerBase
+    {
+        private readonly WorkShopService _workShopService; // readonly evita a edição nas informações
+        public WorkShopsController(WorkShopService workShopService) 
+        {
+            _workShopService = workShopService;
+        }
+        [HttpGet]
+        public async Task<List<WorkShop>> Get() =>
+            await _workShopService.GetAsync();
+
+        [HttpGet("{id:length(24)}")]
+            public async Task<ActionResult<WorkShop>> Get(string id)
+        {
+            var workshop = await _workShopService.GetAsync(id);
+            if (workshop == null)
+            {
+                return NotFound();
+            }
+            return workshop;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(WorkShop newWorkShop)
+        {
+            await _workShopService.CreateAsync(newWorkShop);
+
+            return CreatedAtAction(nameof(Get), new { id = newWorkShop.Id }, newWorkShop);
+        }
+        [HttpPut("{id:length(24)}")]
+        public async Task<IActionResult> Update(string id, WorkShop updatedWorkShop)
+        {
+            var workshop = await _workShopService.GetAsync(id);
+            if (workshop == null)
+
+                return NotFound();
+            updatedWorkShop.Id = workshop.Id;
+            await _workShopService.UpdateAsync(id, updatedWorkShop);
+            return NoContent();
+            
+        }
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var workshop = await _workShopService.GetAsync(id);
+            if (workshop == null)
+                return NotFound();
+            
+            await _workShopService.RemoveAsync(id);
+
+            return NoContent();
+        }
+    }
+}
--- Services/WorkShopService.cs
+++ Services/WorkShopService.cs
@@ -7,25 +7,28 @@ namespace APIQueroOficina.Services
     public class WorkShopService
     {
         private readonly IMongoCollection<WorkShop> _workShopCollection;
-        public  WorkShopService(
+        public WorkShopService(
             IOptions<WorkShopDatabaseSettings> workShopDatabaseSettings)
         {
             var mongoClient = new MongoClient(workShopDatabaseSettings.Value.ConnectionString);
-            var mongoDatabase = mongoClient.GetDatabase((workShopDatabaseSettings.Value.DatabaseName);
+            var mongoDatabase = mongoClient.GetDatabase((workShopDatabaseSettings.Value.DatabaseName));
 
             _workShopCollection = mongoDatabase.GetCollection<WorkShop>(
-                workShopDatabaseSettings.Value.WorkShopCollectionsName );
+                workShopDatabaseSettings.Value.WorkShopCollectionsName);
         }
 
-        public async Task<List<WorkShop>> GetAsync()=> // buscar todos os itens
-            await _workShopCollection.Find(_=>true).ToListAsync();
-        public async Task<WorkShop> GetAsync(string id)=>
+        public async Task<List<WorkShop>> GetAsync() => // buscar todos os itens
+            await _workShopCollection.Find(_ => true).ToListAsync();
+        public async Task<WorkShop?> GetAsync(string id) =>
             await _workShopCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
-        public  async Task CreateAsync(WorkShop newWorkShop) =>
+        public async Task CreateAsync(WorkShop newWorkShop) =>
             await _workShopCollection.InsertOneAsync(newWorkShop);
-        public async async UpdateAsync(string id, WorkShop updateWorkShop) =>
-            await _workShopCollection.ReplaceOneAsync(x => x.Id, updateWorkShop);
+        public async Task UpdateAsync(string id, WorkShop updatedWorkShop) =>
+            await _workShopCollection.ReplaceOneAsync(x => x.Id == id, updatedWorkShop);
         public async Task RemoveAsync(string id) =>
             await _workShopCollection.DeleteOneAsync(x => x.Id == id);
+    }
 
-}
+    }
+
+
